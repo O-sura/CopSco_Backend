eb2e0500565b460e9025c95a0330b11987b2359e
Osura Viduranga <84634723+O-sura@users.noreply.github.com>
2023-09-29 19:30:51 +0530

    Fixed router issue

--- routes/finePaymentRoute.js
+++ routes/finePaymentRoute.js
@@ -3,10 +3,11 @@ const router  = express.Router();
 const roleChecker = require('../middleware/roleChecker');
 const finePaymentController = require('../controllers/finePaymentController');
 
-router.get('/payfine_online', finePaymentController.payfineOnline);
+router.get('/payfine_online', roleChecker('general-user'), finePaymentController.payfineOnline);
 
-router.post('/notify_payment', finePaymentController.updatePaymentStatus);
+router.post('/notify_payment', roleChecker('general-user'), finePaymentController.updatePaymentStatus);
 
-router.post('/payfine_manual', finePaymentController.payfineOffline);
+router.post('/payfine_manual', roleChecker('general-user'), finePaymentController.payfineOffline);
 
 
+module.exports = router;
